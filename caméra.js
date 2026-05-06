109e9bc08c1c17f932e49c8b2f132e22222f950a
Matthias <mat.orbach@gmail.com>
2026-05-06 07:07:15 +0200

    m267

--- caméra.js
+++ caméra.js
@@ -76,14 +76,7 @@ export async function saveDessins(canvasID, fichier, feuille) {
   try {
     log("📸 Taking photo...");
 
-    const video = document.getElementById("video");
     const canvas = document.getElementById(canvasID);
-
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(video, 0, 0);
 
     const image = canvas.toDataURL("image/png");
 
