adf09c79b1fa77c5d02b759fe2a393abbc3d5a13
Matthias <mat.orbach@gmail.com>
2026-05-06 11:10:42 +0200

    m299

--- dessin.js
+++ dessin.js
@@ -115,9 +115,8 @@ function belle(canvasID){
 	console.log(`ratio: ${ratio}\nlargeurSource: ${largeurSource}`);
 	ctx.drawImage(
 		video,
-		0, 0,//
-		//(vw-largeurSource)/2, 0, largeurSource, vh,	//source
-		//0, 0, cw, ch,					//destination
+		(vw-largeurSource)/2, 0, largeurSource, vh,	//source
+		0, 0, cw, ch,					//destination
 	);
 }
 
