9207e93b8e5e99af68b3027e3fd39d8790ea0492
Matthias <mat.orbach@gmail.com>
2026-05-06 09:00:53 +0200

    m278

--- cam.js
+++ cam.js
@@ -24,11 +24,11 @@ function initBoutons(){
 			showEditUI();
 	document.getElementById("btnPhoto")
 		.addEventListener("click", () => {
-			const video = document.getElementById("video");
-			const photo = document.getElementById("photo");
-			const ctx = photo.getContext("2d");
-			photo.width = video.videoWidth;
-			photo.height = video.videoHeight;
+			//const video = document.getElementById("video");
+			//const photo = document.getElementById("photo");
+			//const ctx = photo.getContext("2d");
+			//photo.width = video.videoWidth;
+			//photo.height = video.videoHeight;
 			//ctx.drawImage(video, 0, 0);//
 			//belleImage();
 			initColoriage();
@@ -66,44 +66,6 @@ function concatainer(){//{{{
 	ctxFinal.clearRect(0, 0, dessin.width, dessin.height);
 }//}}}
 
-function belleImage(){//{{{
-	const video = document.getElementById("video");
-	const photo = document.getElementById("photo");
-	const ctx = photo.getContext("2d");
-
-	const vw = video.videoWidth;
-	const vh = video.videoHeight;
-
-	const cw = photo.width;
-	const ch = photo.height;
-
-	// ratio
-	const videoRatio = vw / vh;
-	const photoRatio = cw / ch;
-
-	let sx, sy, sWidth, sHeight;
-
-	if (videoRatio > photoRatio) {
-		// vidéo trop large → crop horizontal
-		sHeight = vh;
-		sWidth = vh * photoRatio;
-		sx = (vw - sWidth) / 2;
-		sy = 0;
-	} else {
-		// vidéo trop haute → crop vertical
-		sWidth = vw;
-		sHeight = vw / photoRatio;
-		sx = 0;
-		sy = (vh - sHeight) / 2;
-	}
-
-	// dessiner avec crop
-	ctx.drawImage(
-		video,
-		sx, sy, sWidth, sHeight, // source
-		0, 0, cw, ch            // destination
-	);
-}//}}}
 
 /*function belle(){//AFINIR
 	const video = document.getElementById("video");
