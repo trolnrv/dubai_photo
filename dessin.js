b2cabef0c52648eb428d415187aefd761eee3661
Matthias <mat.orbach@gmail.com>
2026-05-06 10:28:58 +0200

    m291

--- dessin.js
+++ dessin.js
@@ -110,11 +110,12 @@ function belle(canvasID){
 	const ch = canvas.height;
 
 	const ratio = ch / vh;
-	const largeurSource = ratio/cw;
+	const largeurSource = cw / ratio;
+	//const largeurSource = vh * cw/ch;
 	console.log(`ratio: ${ratio}\nlargeurSource: ${largeurSource}`);
 	ctx.drawImage(
 		video, 
-		(vw-largeurSource)/2, 0, largeurSource, vh,	//source
+		0, /*(vw-largeurSource)/2,*/ 0, largeurSource, vh,	//source
 		0, 0, cw, ch,					//destination
 	);
 }
