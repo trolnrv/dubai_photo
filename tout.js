4e272d19166045675872fa1e2e4baa05e932b66d
Matthias <mat.orbach@gmail.com>
2026-06-28 07:03:40 +0200

    n44

--- tout.js
+++ tout.js
@@ -8,26 +8,29 @@ function ajouterBouton(message, fonction){
 function aller(lien){
 	window.location.href = lien; //window.location.href = `/cam/${id}/${feuille}`;
 }
-ajouterBouton("🔄", () => {
-	refresh();
-});
-const data = await download();
-let fichiers = data.fichiers;
-console.log(fichiers);
-for (let fichier in fichiers){
-	if (fichiers[fichier].type == "D"){
-		ajouterBouton(fichiers[fichier].nom + "📁", () => {
-			aller(`/d/${fichier}`);
-		});
-	}
-	if (fichiers[fichier].type == "F"){
-		ajouterBouton(fichiers[fichier].nom + "📄", () => {
-			aller(`/f/${fichier}`);
-		});
-		for (let feuille in fichiers[fichier].feuilles){
-			ajouterBouton(feuille + "📊", () => {
-			aller(`/cam/${fichier}/${encodeURIComponent(feuille)}`);
+async function charger(){
+	const data = await download();
+	let fichiers = data.fichiers;
+	console.log(fichiers);
+	for (let fichier in fichiers){
+		if (fichiers[fichier].type == "D"){
+			ajouterBouton(fichiers[fichier].nom + "📁", () => {
+				aller(`/d/${fichier}`);
 			});
+		}
+		if (fichiers[fichier].type == "F"){
+			ajouterBouton(fichiers[fichier].nom + "📄", () => {
+				aller(`/f/${fichier}`);
+			});
+			for (let feuille in fichiers[fichier].feuilles){
+				ajouterBouton(feuille + "📊", () => {
+					aller(`/cam/${fichier}/${encodeURIComponent(feuille)}`);
+				});
+			}
 		}
 	}
 }
+ajouterBouton("🔄Charger les nouveaux fichiers🔄", () => {
+	refresh();
+});
+await charger();
