ef781a32f18106040d8f79266c1118c89516a7e4
Matthias <mat.orbach@gmail.com>
2026-06-28 11:42:25 +0200

    n91

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
 	console.log("upload || REQ RECEIVED");
-	const scriptURL = "https://script.google.com/macros/s/AKfycbwQDFIgdB9uQxRfAnJVBQqEqakt7iMLkvcpiGy4Hpj99r2UhcFS3RhDIgwYLTWEG4qZTw/exec";
+	const scriptURL = "https://script.google.com/macros/s/AKfycbwLt9Op9X-YY3sH_aLTn-13xnNYBkV0owgNYH950DO4Hoq1OsUNQH597dnRB_nm68Npgw/exec";
 	// Papa: https://script.google.com/macros/s/AKfycbxIuNF-bBtgZHiCE8b59ZmA3b-3cpYIt3IS4aIhL3w04PXRfpzZYEVQPzP21S9pPrQZ/exec
 
 	const type = req.body.type;
